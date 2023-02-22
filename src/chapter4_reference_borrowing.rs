1ac01e42a83ceae22c4ca0da451e8b452c2426e0
ranafge <ranafge@gmail.com>
2023-02-22 16:15:12 +0600

    ownership return by function

--- src/chapter4_reference_borrowing.rs
+++ src/chapter4_reference_borrowing.rs
@@ -1,4 +1,4 @@
-use std::fmt::format;
+use std::{fmt::format, string};
 
 pub fn reference_borrowing() {
     let m1 = String::from("Hello");
@@ -7,10 +7,24 @@ pub fn reference_borrowing() {
     greet(m1, m2);
     // compile error due ownership chage.
     // let s = format!("m1 is {} and m2 is {}", m1 , m1);
+
+    // returning ownership
+    let string1 = String::from("Hello");
+    let string2 = String::from("World");
+    let (g1_again, g2_again) = return_ownership(string1, string2);
+    let s  = format!("{}-{}", g1_again, g2_again);
+    println!("sssss-{s}")
+
+
 }
 
 // greet function
 
 fn greet(g1: String, g2: String) {
     println!("String one is {} and string two is {}", g1, g2)
+}
+
+fn return_ownership(g1: String, g2: String) -> (String, String) {
+    println!("String1-{} and String2-{}", g1, g2);
+    (g1, g2)
 }
